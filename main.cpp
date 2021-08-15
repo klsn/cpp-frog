0ae5af96fd9820694b015101ba55e7cbe0d210d7
klsn <git@su-shiki.com>
2021-08-16 06:10:55 +0900

    add cost calculation

--- main.cpp
+++ main.cpp
@@ -5,5 +5,12 @@ int main() {
 	cin >> n;
 	vector<int> h(n);
 	for (int i=0;i<n;i++) cin >> h.at(i);
-	for (int i=0;i<n;i++) cout << h.at(i) << endl;
+	int inf = 1 << 30;
+	vector<int> cost(n, inf);
+	cost.at(0) = 0;
+	cost.at(1) = abs(h.at(0) - h.at(1));
+	for (int i=2; i<n; i++) {
+		cost.at(i) = min(cost.at(i-1) + abs(h.at(i) - h.at(i-1)), cost.at(i-2) +abs(h.at(i) - h.at(i-2)));
+	}
+	cout << cost.at(n-1) << endl;
 }
